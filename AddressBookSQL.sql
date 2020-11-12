26ef8106673ae735630371d71ac663ddc5d6ebcb
ShravyaHub <shravyak710@gmail.com>
2020-11-12 13:55:40 +0530

    ADDED: created table AddressBook with attributes FirstName, LastName, Address, City, State, Zip, PhoneNumber and Email

--- AddressBookSQL.sql
+++ AddressBookSQL.sql
@@ -1,2 +1,13 @@
 CREATE DATABASE AddressBookService;
 USE AddressBookService;
+
+CREATE TABLE AddressBook(
+    FirstName VARCHAR(20) NOT NULL PRIMARY KEY,
+    LastName VARCHAR(20) NOT NULL,
+    Address VARCHAR(100) NOT NULL,
+    City VARCHAR(20) NOT NULL,
+    State VARCHAR(20) NOT NULL,
+    Zip INT(6) NOT NULL,
+    PhoneNumber INT(12) NOT NULL,
+    Email VARCHAR(50) NOT NULL
+);
